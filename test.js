00fcf42587336b9aab5973e0a6cbe7e060c2f56e
aminopyrodin <aminopyrodin@gmail.com>
2017-06-08 20:35:11 +0500

    initial commit

---
+++ test.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import Life from './src/Life.js';
+
+describe('Life', function() {
+	it('should ...', function() {
+		
+	});
+	it('should ...', function() {
+		
+	});
+});
