e0bfc8fe917ff49935ddf432a793737867313a63
aminopyrodin <aminopyrodin@gmail.com>
2017-06-08 21:07:41 +0500

    test example

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 import assert from 'assert';
 import Life from './src/Life.js';
 
-describe('Life', function() {
-	it('should ...', function() {
-		
-	});
-	it('should ...', function() {
-		
-	});
+describe('Life', function () {
+    it('should ...', function () {
+
+    });
+    it('should ...', function () {
+
+    });
 });
+
+
+// example
+/*
+describe('willBeAlive', function () {
+    it('should be dead if <2', function () {
+        const neighbours = 1;
+        const state = true;
+        assert.equal(willBeAlive(neighbours, state), false);
+    });
+});
+*/
